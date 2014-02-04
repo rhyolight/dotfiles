5065d8e9e1fcb84f78bb7a87bf37af749d2eb486
Matthew Taylor <rhyolight@gmail.com>
2014-02-04 09:54:37 -0800

    Aliases and stuff

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -19,3 +19,5 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
 alias gpum='git pull upstream master'
+
+alias git=hub
