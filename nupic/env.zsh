e04dee2c2442b99cc21adf684caa3baf7d37b533
Matthew Taylor <rhyolight@gmail.com>
2014-02-19 09:35:30 -0800

    cpp compiler

--- nupic/env.zsh
+++ nupic/env.zsh
@@ -1,17 +1,20 @@
-export NTA=~/nta
+# Installation path
+export NTA=$HOME/nta/eng
+# Target source/repo path. Defaults to $PWD
+export NUPIC=$HOME/nta/nupic
+# Convenience variable for temporary build files
+export BUILDDIR=/tmp/ntabuild
+# Number of jobs to run in parallel (optional)
+export MK_JOBS=3
+
+# Set up the rest of the necessary env variables. Must be done after
+# setting $NTA.
+source $NUPIC/env.sh
 
 # Location of numenta.org source code
 export NUMENTA_ORG=$NTA/numenta.org
 # Location of numenta/nupic.core checkout
 export NUPIC_CORE=$NTA/nupic.core
-# Target source/repo path (where you cloned the repo). Defaults to $PWD
-export NUPIC=$NTA/nupic
-# NTA installation path
-export NTA=$NUPIC/nta/eng
-# Convenience variable for temporary build files
-export BUILDDIR=/tmp/ntabuild
-# Number of jobs to run in parallel (optional)
-export MK_JOBS=3
-# Set up the rest of the necessary env variables. Must be done after
-# setting $NTA.
-source $NUPIC/env.sh
+
+# Location of CPP compiler for Nupic
+export CXX=/usr/bin/clang
