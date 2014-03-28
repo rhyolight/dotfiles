4c9aa9310aef655ebf848b5a312349ba9928b5a6
Matthew Taylor <rhyolight@gmail.com>
2014-03-28 09:03:06 -0700

    new aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -19,5 +19,6 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
 alias gpum='git pull upstream master'
+alias gpug='git pull upstream gh-pages'
 
 alias git=hub
--- nupic/aliases.zsh
+++ nupic/aliases.zsh
@@ -1,6 +1,7 @@
 alias nta='cd ~/nta'
 alias npc='cd $NUPIC'
 alias wall='cd ~/nta/nupic.wallboard'
+alias nwiki='cd ~/nta/nupic.wiki'
 alias ntools='cd ~/nta/nupic.tools'
 alias norg='cd ~/nta/numenta.org'
 alias ncore='cd ~/nta/nupic.core'
