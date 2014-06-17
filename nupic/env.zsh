c366804fa737d54c2c962c340a5d6558ff105d6e
Matthew Taylor <rhyolight@gmail.com>
2014-06-17 08:23:56 -0700

    General updates

--- nupic/env.zsh
+++ nupic/env.zsh
@@ -1,18 +1,11 @@
 # Installation path
 export NUPIC=$HOME/nta/nupic
-export NTA=$NUPIC/build/release
-export NTA_ROOT_DIR=$NTA
-export PYTHONPATH=$PYTHONPATH:$NTA/lib/python2.7/site-packages
 
 # Location of numenta.org source code
 export NUMENTA_ORG=$NTA/numenta.org
 # Location of numenta/nupic.core checkout
 export NUPIC_CORE=$NTA/nupic.core
 
-# export CXX=/usr/bin/clang++
-# export CXX=g++
-#export CC=gcc
-#export CXX=g++
-export CC=clang
-export CXX=clang++
+#export CC=clang
+#export CXX=clang++
 
--- python/path.zsh
+++ python/path.zsh
@@ -1,2 +1,2 @@
 export PATH=/usr/local/Cellar/python/2.7.6/bin:$PATH
-export PYTHONPATH=/Library/Python/2.7/site-packages
+export PYTHONPATH=$PYTHONPATH:/Library/Python/2.7/site-packages
