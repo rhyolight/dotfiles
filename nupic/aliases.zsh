d0279becfab08545a899e60f61a999e643512a20
Matthew Taylor <matt@numenta.org>
2016-02-22 08:45:14 -0800

    Updates

--- nupic/aliases.zsh
+++ nupic/aliases.zsh
@@ -1,3 +1,4 @@
+source ${HOME}/.dotfiles/nupic/env.zsh
 alias nta='cd ~/nta'
 alias npc='cd $NUPIC'
 alias nab='cd $NTA/nab'
@@ -5,7 +6,7 @@ alias wall='cd $NTA/nupic.wallboard'
 alias nwiki='cd $NTA/nupic.wiki'
 alias ntools='cd $NTA/nupic.tools'
 alias norg='cd $NTA/numenta.org'
-alias ncore='cd $NTA/nupic.core'
+alias ncore='cd $NUPIC_CORE'
 alias cortipy='cd $NTA/cortipy'
 alias htmjava='cd $NTA/htm.java'
 alias research='cd $NTA/nupic.research'
@@ -14,12 +15,11 @@ alias htm='cd $NTA/htmengine-traffic-tutorial'
 alias rv='cd ~/dev/river-view'
 alias napps='cd $NTA/numenta-apps'
 alias hitc='cd $NTA/hitc'
+alias smart='cd $NTA/smartthings.htm.bridge'
 
 # Build helpers
-alias build-nupic-core="(cd ${HOME}/nta/nupic.core && mkdir -p build/scripts && cd build/scripts && cmake ../../src -DCMAKE_INSTALL_PREFIX=${HOME}/nta/nupic.core/build/release && make -j6 install)"
-
-alias build-nupic-bindings="(cd ${HOME}/nta/nupic.core/bindings/py && ARCHFLAGS=\"-arch x86_64\" python setup.py develop --user --nupic-core-dir=${HOME}/nta/nupic.core/build/release)"
-
-alias build-nupic="(cd ${HOME}/nta/nupic && python setup.py develop --user)"
-
+alias build-nupic-core="(mkdir -p ${NUPIC_CORE}/build/scripts && cd ${NUPIC_CORE}/build/scripts && cmake ${NUPIC_CORE} -DCMAKE_INSTALL_PREFIX=../release -DPY_EXTENSIONS_DIR=${NUPIC_CORE}/bindings/py/nupic/bindings && make -j6 install)"
+alias build-nupic-bindings="(cd ${NUPIC_CORE} && ARCHFLAGS=\"-arch x86_64\" python setup.py develop --user)"
+alias build-nupic-core-all="build-nupic-core; build-nupic-bindings"
+alias build-nupic="(cd ${NUPIC} && python setup.py develop --user)"
 alias build-nupic-all="build-nupic-core; build-nupic-bindings; build-nupic"
--- nupic/env.zsh
+++ nupic/env.zsh
@@ -1,19 +1,15 @@
 # NuPIC
 export NTA=$HOME/nta
 export NUPIC=$NTA/nupic
+# Location of numenta/nupic.core checkout
+export NUPIC_CORE=$NTA/nupic.core
+
 # For OS X build
 export ARCHFLAGS="-arch x86_64"
 
 # Location of numenta.org source code
 export NUMENTA_ORG=$NTA/numenta.org
-# Location of numenta/nupic.core checkout
-export NUPIC_CORE=$NTA/nupic.core
-
-# Nupic Studio
-export NUPIC_STUDIO=$NTA/nupic.studio
 
 # HTM.java
 export HTM_JAVA=$NTA/htm.java
 
-# export CC=clang
-# export CXX=clang++
